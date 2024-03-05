d37393f68c59b818fcd1af80b1908e23ce645c25
srijit-ops <srijitasengupta23@gmail.com>
2024-03-05 14:11:14 +0530

    ad widget done without responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react'
+
+//components
+
+import Layout from './components/layout/Layout'
+import KoinxAd from "./components/KoinxAd";
+import theme from "../theme";
+
+export default function App() {
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Layout>
+      <div css={container}>
+
+        <div css={coin_name_holder}>
+          <p css={coin_name_head}>Cryptocurrencies</p>
+          <img src="/headArrow.svg" alt="" />
+          <p css={coin_name}>Bitcoin</p>
+        </div>
+
+        <div css={widgets_holder}>
+          <div css={parent_widget1}>hii</div>
+          <div css={parent_widget2}>
+            <KoinxAd/>
+          </div>
+          <div css={parent_widget3}>hola</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Layout>
   )
 }
 
-export default App
+
+//css
+
+const container = css`
+padding: 1rem 3.6rem ;
+@media screen and (max-width: 900px) {
+  padding: 1.4rem 0.9rem;
+}
+`
+
+const coin_name_holder = css`
+  display: flex;
+  justify-content: flex-start;
+  gap: 0.4rem;
+  align-items: center;
+  flex-direction: row;
+`
+
+const coin_name_head = css`
+  font-size: 0.9rem;
+  color: ${theme.fonts.light_grey};
+  font-weight: 400;
+`
+const coin_name = css`
+  font-size: 0.9rem;
+  color: ${theme.primary_color};
+  font-weight: 500;
+`
+const widgets_holder = css`
+  display: flex;
+  justify-content: space-around;
+  gap: 0.3rem;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+const parent_widget1 = css`
+  width: 65%;
+order: 1;
+  @media screen and (max-width: 900px) {
+        width: 100%;
+    }
+`
+const parent_widget2 = css`
+  width: 28%;
+order: 2;
+  @media screen and (max-width: 900px) {
+        width: 100%;
+        order: 3;
+    }
+`
+const parent_widget3 = css`
+  width: 100%;
+order: 3;
+  @media screen and (max-width: 900px) {
+        width: 100%;
+        order: 2;
+    }
+`
